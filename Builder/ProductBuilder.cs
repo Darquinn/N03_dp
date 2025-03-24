84cf92e2f1fb5810ea6f1b501891e6f337628de1
Darquinn <phthienlann@gmail.com>
2025-03-25 01:11:41 +0700

    add builder pattern

---
+++ Builder/ProductBuilder.cs
@@ -0,0 +1,96 @@
+﻿using fastwebsite.Entities;
+
+namespace fastwebsite.Builder
+{
+    //Builder Pattern - Lân
+    public class ProductBuilder
+    {
+        private int _productId;
+        private string _productName = string.Empty;
+        private decimal _price;
+        private string? _img;
+        private string? _des;
+        private int? _cateId;
+        private ICollection<CartItem> _cartItems = new List<CartItem>();
+        private Category? _cate;
+        private ICollection<OrderItem> _orderItems = new List<OrderItem>();
+        private ICollection<Review> _reviews = new List<Review>();
+
+        public ProductBuilder SetProductId(int productId)
+        {
+            _productId = productId;
+            return this;
+        }
+
+        public ProductBuilder SetProductName(string productName)
+        {
+            _productName = productName;
+            return this;
+        }
+
+        public ProductBuilder SetPrice(decimal price)
+        {
+            _price = price;
+            return this;
+        }
+
+        public ProductBuilder SetImage(string? img)
+        {
+            _img = img;
+            return this;
+        }
+
+        public ProductBuilder SetDescription(string? des)
+        {
+            _des = des;
+            return this;
+        }
+
+        public ProductBuilder SetCategoryId(int? cateId)
+        {
+            _cateId = cateId;
+            return this;
+        }
+
+        public ProductBuilder SetCategory(Category? cate)
+        {
+            _cate = cate;
+            return this;
+        }
+
+        public ProductBuilder AddCartItem(CartItem cartItem)
+        {
+            _cartItems.Add(cartItem);
+            return this;
+        }
+
+        public ProductBuilder AddOrderItem(OrderItem orderItem)
+        {
+            _orderItems.Add(orderItem);
+            return this;
+        }
+
+        public ProductBuilder AddReview(Review review)
+        {
+            _reviews.Add(review);
+            return this;
+        }
+
+        public Product Build()
+        {
+            return new Product
+            {
+                ProductId = _productId,
+                ProductName = _productName,
+                Price = _price,
+                Img = _img,
+                Des = _des,
+                CateId = _cateId,
+                Cate = _cate,
+                CartItems = _cartItems,
+                OrderItems = _orderItems,
+                Reviews = _reviews
+            };
+        }
+    }
+}
